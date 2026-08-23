fbfa6f439b2afa3328c394a122d789ddfce9f5ee
ckaformation <ckaformationpro@gmail.com>
2026-08-23 16:42:48 +0000

    clean up

--- cluster-architecture-installation-configuration/rbac-cronjob/step4-verify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-RESULT=$(kubectl auth can-i delete pods --as=system:serviceaccount:ops:leon-2 -n ops 2>/dev/null)
-if [ "$RESULT" != "yes" ]; then
-  echo "leon-2 devrait pouvoir delete pods dans ops (obtenu: $RESULT)"
-  exit 1
-fi
-echo "leon-2 dispose bien des droits delete sur les pods dans ops."
-exit 0
